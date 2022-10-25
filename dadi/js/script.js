d8ee771387ef1c239817b1da5a6cbce9b94fc3e4
LudwigVonEffort <dr.feynman65@gmail.com>
2022-10-25 20:36:12 +0200

    script dice done

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -4,4 +4,29 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
+const rollDice = document.getElementById('btnDice');
 
+rollDice.addEventListener('click',
+
+    function () {
+
+        let usrNum = Math.floor((Math.random() * 10) + 1);
+        let skyNum = Math.floor((Math.random() * 10) + 1);
+
+        document.getElementById('userNumber').innerHTML = usrNum;
+        document.getElementById('skynetNumber').innerHTML = (skyNum);
+
+        if (usrNum > skyNum) {
+            document.getElementById('winner').innerHTML = "Mankind wins";
+        }
+
+        else if (usrNum < skyNum) {
+            document.getElementById('winner').innerHTML = "Skynet wins";
+        } 
+        
+        else {
+            document.getElementById('winner').innerHTML = "Draw";
+        }
+    }
+
+);
